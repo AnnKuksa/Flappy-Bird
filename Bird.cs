59c6f1edfb1c3477a1e6d2dbe09db062b6178694
AnnKuksa <78798097+AnnKuksa@users.noreply.github.com>
2021-02-09 14:59:49 +0300

    Add files via upload

---
+++ Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float velocity = 1; // скорость птички
+    private Rigidbody2D rb; // птичка
+    public GameManager gameManager;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) // если нажимать на кнопку
+        {
+            rb.velocity = Vector2.up * velocity; // птичка летит вверх
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GamOver();
+    }
+}
